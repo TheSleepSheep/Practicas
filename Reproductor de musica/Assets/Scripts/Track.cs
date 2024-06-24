14d399168ce5a7c7d41bbfe73a3f310cac6d1504
TheSleepSheep <castillomogueko@gmail.com>
2024-06-24 00:24:36 -0600

    CreacionProyectoMusica
    Se creo un proyecto en Unity

---
+++ Reproductor de musica/Assets/Scripts/Track.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+
+public class Track : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public TMP_Text clipName;
+    public int idTrack;
+
+    void Start()
+    {
+        try
+        { 
+        clipName.text = clip.name;
+        audioSource = FindObjectOfType<AudioSource>();
+        }
+        catch
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+
+    public void PlayTrack()
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+        ReproductorAudio.instance.currentTrack = idTrack;
+
+    }
+    
+}
