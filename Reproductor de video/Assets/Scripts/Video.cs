95894a807f91d6a5694eb49cf6581ac1af192f55
TheSleepSheep <castillomogueko@gmail.com>
2024-07-01 20:54:53 -0600

    arreglos menores

--- Reproductor de video/Assets/Scripts/Video.cs
+++ Reproductor de video/Assets/Scripts/Video.cs
@@ -5,17 +5,51 @@ using UnityEngine.Video;
 
 public class Video : MonoBehaviour
 {
-    public VideoPlayer _videoPlayer;
+    public VideoPlayer video;
+    public List<VideoClip>video_01 = new List<VideoClip>();
+    public Transform trackContainer;
+    public GameObject listaobj;
+    public int currentTrack;
 
-    public VideoClip clipVideo;
-    
     void Start()
     {
-        _videoPlayer.clip = clipVideo;
+        for (int i = 0; i < video_01.Count; i++)
+        {
+            Instantiate(listaobj, trackContainer);
+            listaobj.GetComponent<TrackVideo>().clip = video_01[i];
+        }
     }
 
-    void Update()
+    public void Forward()
     {
-        
+        if (currentTrack < video_01.Count -1)
+        {
+            currentTrack++;
+            video.clip = video_01[currentTrack];
+            video.Play();
+        }
+        else
+        {
+            currentTrack = 0;
+            video.clip = video_01[0];
+            video.Play();
+        }
     }
+
+    public void Backward()
+    {
+        if(currentTrack < 0)
+        {
+            currentTrack--;
+            video.clip = video_01[currentTrack];
+            video.Play();   
+        }
+        else
+        {
+            currentTrack = video_01.Count -1;
+            video.clip  = video_01[currentTrack];
+            video.Play();
+        }
+    }
+
 }
